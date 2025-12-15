cf3830b069ddab557f26d28e746e0ed3dcff8259
o1key222k <o1key222k@gmail.com>
2025-12-15 23:31:05 +0700

    feat/brand-api: create and test api route for brand model

--- types/schemas.ts
+++ types/schemas.ts
@@ -9,29 +9,19 @@ export const tokenValidationSchema: ZodType<TokenFormData> = z.object({
   status: z.enum(["draft", "publish", "archive"]).optional(),
 });
 
-// export const brandValidationSchema: ZodType<BrandFormData> = z.object({
-//   name: z.string().regex(nameFormat, {
-//     message: "format: no special (&/,), min 4 and max 60 characters",
-//   }),
-//   slug: z.string().regex(slugFormat, {
-//     message:
-//       "format: no blank space (_), no special character (&,',/|...), min 4 and max 20 characters",
-//   }),
-//   description: z.string().regex(descriptionFormat, {
-//     message: "format:min 4 and max 500 characters",
-//   }),
-//   image: z.string().url("Image url is invalid"),
-//   // image: z.object({ url: z.string() }).array(),
-//   status: z.enum(["draft", "publish", "archive"]),
-//   user_id: z.string().optional().nullable(),
-// });
-
 export const brandValidationSchema: ZodType<BrandFormData> = z.object({
-  name: z.string(),
-  slug: z.string(),
-  description: z.string().optional(),
-  image: z.string(),
+  name: z.string().regex(nameFormat, {
+    message: "format: no special (&/,), min 4 and max 60 characters",
+  }),
+  slug: z.string().regex(slugFormat, {
+    message:
+      "format: no blank space (_), no special character (&,',/|...), min 4 and max 20 characters",
+  }),
+  description: z.string().regex(descriptionFormat, {
+    message: "format:min 4 and max 500 characters",
+  }),
+  image: z.string().url("Image url is invalid"),
   // image: z.object({ url: z.string() }).array(),
-  status: z.enum(["draft", "publish", "archive"]).optional(),
+  status: z.enum(["draft", "publish", "archive"]),
   user_id: z.string().optional().nullable(),
 });
