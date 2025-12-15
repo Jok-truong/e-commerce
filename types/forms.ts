9837915690cf9a98c9d904ceb3ef5dfc6e2a0e72
o1key222k <o1key222k@gmail.com>
2025-12-15 23:23:51 +0700

    feat/brand-api: create and test api route for brand model

--- types/forms.ts
+++ types/forms.ts
@@ -3,3 +3,14 @@ export type TokenFormData = {
   token: string;
   user_id: string;
 };
+
+export type BrandFormData = {
+  _id?: string;
+  name: string;
+  slug: string;
+  description?: string;
+  image: string;
+  status: "draft" | "publish" | "archive";
+  user_id?: string | null | undefined;
+  createdAt?: string;
+};
